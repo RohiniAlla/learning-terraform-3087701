77ce3d1afc03d2a70e50530223b34e075ea82ecf
RohiniAlla <133994292+RohiniAlla@users.noreply.github.com>
2023-06-01 14:00:34 -0500

    added security groups in module

--- main.tf
+++ main.tf
@@ -32,31 +32,14 @@ module "blog_sg" {
   version = "5.0.0"
   name = "blog_new"
   vpc_id = data.aws_vpc.default.id
+  
   ingress_rules = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.blog.id
 
   egress_rules = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
-}
 
-resource "aws_security_group" "blog" {
-  name        = "blog"
-  description = "allow http and https in. Allow everything out"
-  vpc_id = data.aws_vpc.default.id
-}
-resource "aws_security_group_rule" "blog_http_in" {
-  type = "ingress"
-  from_port = 80
-  to_port = 80
-  protocol = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
   security_group_id = aws_security_group.blog.id 
 }
-resource "aws_security_group_rule" "blog_http_out" {
-  type = "egress"
-  from_port = 0
-  to_port = 0
-  protocol = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.blog.id
-}
+
