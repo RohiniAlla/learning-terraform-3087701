fba8c56012a386dfc420c9314de8ed341454f4f5
RohiniAlla <133994292+RohiniAlla@users.noreply.github.com>
2023-06-01 22:43:58 -0500

    made changes for VPC

--- main.tf
+++ main.tf
@@ -45,7 +45,7 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.0.0"
   name = "blog_new"
-  vpc_id = module.vpc.public_subnets
+  vpc_id = module.vpc.public_subnets[0]
 
   ingress_rules = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
@@ -53,36 +53,6 @@ module "blog_sg" {
   egress_rules = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 
-  security_group_id = aws_security_group.blog.id 
-}
-resource "aws_security_group" "blog" {
-  name        = "blog"
-  description = "allow http and https in. Allow everything out"
-  vpc_id = data.aws_vpc.default.id
-}
-resource "aws_security_group_rule" "blog_http_in" {
-  type        = "ingress"
-  from_port   = 80
-  to_port     = 80
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.blog.id 
-}
-resource "aws_security_group_rule" "blog_https_in" {
-  type        = "ingress"
-  from_port   = 443
-  to_port     = 443
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.blog.id 
-}
-resource "aws_security_group_rule" "blog_http_out" {
-  type        = "egress"
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.blog.id
 }
 
 
