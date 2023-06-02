184520a1cd39b72e546119c4994f0e1f2905ddfc
RohiniAlla <133994292+RohiniAlla@users.noreply.github.com>
2023-06-01 23:08:19 -0500

    added changes to VPC_id

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ data "aws_ami" "app_ami" {
 
   owners = ["979382823631"] # Bitnami
 }
-module "blg_vpc" {
+module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "dev"
@@ -48,8 +48,8 @@ module "alb" {
 
   load_balancer_type = "application"
 
-  vpc_id             = module.blg_vpc.vpc_id
-  subnets            = module.blg_vpc.public_subnets
+  vpc_id             = module.blog_vpc.vpc_id
+  subnets            = module.blog_vpc.public_subnets
   security_groups    = [module.blog_sg.security_group_id]
 
   
@@ -87,7 +87,7 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.0.0"
   name = "blog_new"
-  vpc_id = module.vpc.public_subnets[0]
+  vpc_id = module.blog_vpc.public_subnets[0]
 
   ingress_rules = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
